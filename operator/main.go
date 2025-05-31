0efc79e3dca89851576788e8abc91cd8d7c1a96b
Aizu <mfaiz2035@gmail.com>
2025-05-31 20:49:12 +0700

    operator

--- operator/main.go
+++ operator/main.go
@@ -4,5 +4,15 @@ import "fmt"
 
 func main() {
 	var value = (((2+6)%3)*4 - 2) / 3
-	fmt.Println(value)
+	var isEqual = (value == 2)
+	fmt.Printf("nilai %d (%t) \n", value, isEqual)
+
+	var left = false
+	var right = true
+	var leftAndRight = left && right
+	fmt.Printf("left && right \t(%t) \n", leftAndRight)
+	var leftOrRight = left || right
+	fmt.Printf("left || right \t(%t) \n", leftOrRight)
+	var leftReverse = !left
+	fmt.Printf("!left \t\t(%t) \n", leftReverse)
 }
