6325c956158ee92020e1a57ec8d41b8271e11d92
Aizu <mfaiz2035@gmail.com>
2025-05-31 20:21:22 +0700

    variable

--- variable/main.go
+++ variable/main.go
@@ -6,6 +6,16 @@ func main() {
 	var firstName string = "John"
 	var lastName string
 	lastName = "Wick"
-
 	fmt.Printf("Halo %s %s!\n", firstName, lastName)
+
+	person := "Saya"
+	fmt.Printf("%s adalah programmer!\n", person)
+
+	_ = "unused"
+	_, secondVar := "ignored", "used"
+	fmt.Println(secondVar)
+
+	name := new(string)
+	fmt.Println(name)
+	fmt.Println(*name)
 }
